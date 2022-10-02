1f10a474041a63544c737013156baa54da687220
Maxim Tymchyshyn <mskstimka@gmail.com>
2022-10-02 19:08:39 +0300

    feature (part 1) - save point

--- app/src/main/java/com/example/animator/details/ui/DetailsFragment.kt
+++ app/src/main/java/com/example/animator/details/ui/DetailsFragment.kt
@@ -2,6 +2,7 @@ package com.example.animator.details.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -182,13 +183,16 @@ class DetailsFragment : Fragment() {
         subscribeToFlow(
             flow = actionVideo,
             lifecycleOwner = viewLifecycleOwner
-        ) { item ->
-            binding.btWatch.visibility = MaterialButton.VISIBLE
-            binding.btWatch.setOnClickListener {
-                findNavController().navigate(
-                    DetailsFragmentDirections.actionDetailsFragmentToWebPlayerFragment(item.first().url)
-                )
+        ) { list ->
+            if (list.isNotEmpty()) {
+                binding.btWatch.visibility = MaterialButton.VISIBLE
+                binding.btWatch.setOnClickListener {
+                    findNavController().navigate(
+                        DetailsFragmentDirections.actionDetailsFragmentToWebPlayerFragment(list.first().url)
+                    )
+                }
             }
+
         }
 
 
